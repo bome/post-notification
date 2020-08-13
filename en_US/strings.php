485fd8daae0e9d1f3869e8afbf50375782e9b279
bome <github@bome.com>
2020-08-14 01:39:38 +0200

    improve English language

--- en_US/strings.php
+++ en_US/strings.php
@@ -1,19 +1,19 @@
 <?php
 $post_notification_strings = array(
 	'error' => 'Error',
-	'already_subscribed' => 'You already subscribed!',
+	'already_subscribed' => 'You have already subscribed.',
 	'activation_faild' => 'Activation failed',
-	'address_not_in_database' => 'Your address is not in our Database.',
+	'address_not_in_database' => 'Your email address could not be found in our database.',
 	'sign_up_again'=>'Please sign up again.',
 	'deaktivated' => 'Deactivated',
-	'no_longer_activated' => 'The E-mail address @@addr in no longer activated on @@blogname.',
-	'check_email' => 'Please enter your correct E-mail address and check the spelling.',
-	'wrong_captcha' => 'Unfortunately, the characters you typed do not match those in the CAPTCHA image.',
+	'no_longer_activated' => 'The email address @@addr in no longer subscribed on @@blogname.',
+	'check_email' => 'Please enter your correct email address and check the spelling.',
+	'wrong_captcha' => 'Unfortunately, the security code you typed is not correct. Please try again.',
 	'registration_successful' => 'Registration successful',
 	'activated' => 'Activation successful',
 	'all' => 'All',
 	'saved' => 'The settings were saved.',
-	'unsubscribe_mail' => 'You\'ve got a mail with a deactivation Link.',
+	'unsubscribe_mail' => 'Please check your email inbox for the email with the deactivation Link.',
 	'welcome' => 'You have just subscribed to @@blogname.',
 )
 ?>
